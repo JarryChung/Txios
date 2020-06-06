56bcf175441ec81bc576653ff11058e19e50b0cb
JarryChung <jarrychung@outlook.com>
2020-06-06 11:13:21 +0800

    feat(main): create an instance, a function with Txios properties

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * 导出 txios 实例以及类型
+ */
+
+import txios from './txios'
+
+export * from './types'
+
+export default txios
--- src/txios.ts
+++ src/txios.ts
@@ -2,4 +2,23 @@
  * 入口文件
  */
 
-export default 'txios'
+import { TxiosInstance } from './types'
+import Txios from './core/Txios'
+import { extend } from './utils'
+
+// 工厂模式，创建一个方法 instance，并将 Txios 实例上的属性挂载到 instance 上
+function createInstance(): TxiosInstance {
+  // 创建 Txios 实例
+  const context = new Txios()
+  // instance 变量存储的是 Txios 类中的 request 方法
+  const instance = Txios.prototype.request.bind(context)
+  // 将 Txios 实例上的属性挂载到 instance 上
+  extend(instance, context)
+  // 返回该方法
+  return instance as TxiosInstance
+}
+
+// 利用工厂方法创建实例
+const txios = createInstance()
+// 导出
+export default txios
